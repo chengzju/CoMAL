b87a0c395e4deb697565c6303ec1fb0b6981a1f4
Cheng Peng <153715087+chengzju@users.noreply.github.com>
2024-06-08 16:55:09 +0800

    Add files via upload

---
+++ scripts/jd/main_adaptive.sh
@@ -0,0 +1,22 @@
+python ./main.py \
+--data_dir ./data/jd \
+--maxlength 64 \
+--batch_size 64 \
+--unlab_batch_size 64 \
+--test_size 1500 \
+--total_patience 6 \
+--save_path ./model_saved/jd \
+--lr 1e-4 \
+--gpuid '1' \
+--cycles 51 \
+--try_id 1 \
+--seed 1 \
+--method_type adaptive \
+--init_example_num 50 \
+--well_init_lower_bound 1 \
+--sample_pair_num 50 \
+--dynamic_split \
+--test_data_size 5000 \
+--well_init \
+--freeze_bert \
+--freeze_layer_num 9 \
---
+++ scripts/jd/main_our.sh
@@ -0,0 +1,28 @@
+python ./main.py \
+--data_dir ./data/jd \
+--maxlength 64 \
+--batch_size 64 \
+--unlab_batch_size 64 \
+--test_size 1500 \
+--total_patience 6 \
+--save_path ./model_saved/jd \
+--lr 1e-4 \
+--gpuid '0' \
+--cycles 51 \
+--try_id 1 \
+--seed 1 \
+--method_type chmlal \
+--init_example_num 50 \
+--well_init_lower_bound 1 \
+--sample_pair_num 50 \
+--dynamic_split \
+--test_data_size 5000 \
+--well_init \
+--freeze_bert \
+--freeze_layer_num 9 \
+--train_vae_module \
+--use_recon_bce_loss \
+--use_recon_mse_loss \
+--cl_neg_mode 1 \
+--proto_size 64 \
+--epochs 1 \
